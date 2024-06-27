778a4a54c757df58f8327bc2c9bfcd6a1dbf2352
agorohov <aleksandr.gorohov@gmail.com>
2024-06-27 11:20:56 +0300

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import integerlist.IntegerList;
+import integerlist.IntegerListRealization;
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
+        IntegerList list = new IntegerListRealization();
+        for (int i = 0; i < 50; i++) {
+            list.add(new Random().nextInt(1000));
+        }
+        System.out.println(Arrays.toString(list.toArray()));
+        list.clear();
     }
 }
